fc789f71eca6ed6aaa568e6523482209c4c3d7a7
john33mcd <jmcd-34@hotmail.com>
2022-12-20 21:23:18 +0000

    added functionality to add rows to the spreadsheet

--- run.py
+++ run.py
@@ -22,13 +22,14 @@ def get_sales_data():
         print("Example: 10,20,30,40,50,60\n")
 
         data_str = input("Enter your data here: ")
-        
+
         sales_data = data_str.split(",")
 
         if validate_data(sales_data):
             print("data is valid!")
             break
-
+    
+    return sales_data
 
 def validate_data(values):
 
@@ -48,4 +49,15 @@ def validate_data(values):
 
     return True
 
-get_sales_data()
+def update_sales_worksheet(data):
+
+    """
+    Update sales worksheet, add new row with new list data
+    """
+    print("updating sales worksheet....\n")
+    sales_worksheet = SHEET.worksheet("sales")
+    sales_worksheet.append_row(data)
+    print("sales worksheet updated successfully.\n")
+data = get_sales_data()
+sales_data = [int(num) for num in data]
+update_sales_worksheet(sales_data)
